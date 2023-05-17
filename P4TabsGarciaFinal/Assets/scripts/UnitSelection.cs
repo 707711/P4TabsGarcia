7024c87c79941e1fee1ffe57a050eca7f82137e6
GaboManu <manuelsrandomvideos@gmail.com>
2023-05-17 11:43:46 -0700

    Adding the unit select script

---
+++ P4TabsGarciaFinal/Assets/scripts/UnitSelection.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelection : MonoBehaviour
+{
+
+    public List<GameObject> unitList - new List<GameObject>();
+    public List<GameObject> unitSelected - new List<GameObject>();
+
+    private static unitSelections instance;
+    publicc static unitSelections instance { get { return_instance; } }
+
+    void Awake()
+    {
+        if (instance! - null && instance! - this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public void Clickselect(GameObject unitToAdd)
+    {
+
+    }
+
+    public void shiftClickselect (GameObject unitToAdd)
+    {
+
+    }
+}
