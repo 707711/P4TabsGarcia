45ed9256f38def0a19d80d80b6097e6def5e9351
707711 <707711@student.dusd.net>
2023-05-19 11:59:57 -0700

    Enemy and player scrpit
    cvdsv

---
+++ P4TabsGarciaFinal/Assets/scripts/EnemyScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Transform player;
+    public float enemySpeed, dis;
+    Vector3 startPos;
+
+    private NavMeshAgent navMeshAgent;
+    //private Transform movePositionTransform;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dis = Vector3.Distance(transform.position, player.position);
+        if (dis <= 10.0f)
+        {
+            chase();
+        }
+
+        //navMeshAgent.destination = movePositionTransform.position;
+    }
+
+    void chase()
+    {
+        transform.LookAt(player);
+        transform.Translate(0, 0, enemySpeed * Time.deltaTime);
+    }
+}
--- P4TabsGarciaFinal/Assets/scripts/UnitSelection.cs
+++ P4TabsGarciaFinal/Assets/scripts/UnitSelection.cs
@@ -6,11 +6,20 @@ public class UnitSelection : MonoBehaviour
 {
 
     //public List<GameObject> unitList - new List<GameObject>();
+<<<<<<< Updated upstream
    //public List<GameObject> unitSelected - new List<GameObject>();
+=======
+<<<<<<< HEAD
+    //public List<GameObject> unitSelected - new List<GameObject>();
+=======
+   //public List<GameObject> unitSelected - new List<GameObject>();
+>>>>>>> 8e323b4824ffe4c23386b0352c4b83ab3f2fd66b
+>>>>>>> Stashed changes
 
     //private static unitSelections instance;
     //public static unitSelections instance { get { return_instance; } }
 
+<<<<<<< Updated upstream
    // void Awake()
     //{
        // if (instance! - null && instance! - this)
@@ -28,8 +37,54 @@ public class UnitSelection : MonoBehaviour
 
    // }
 
+=======
+<<<<<<< HEAD
+    void Awake()
+    {
+        //if (instance! - null && instance! - this)
+        {
+            //Destroy(this.gameObject);
+        }
+        //else
+        {
+            //instance = this;
+        }
+    }
+
+    //public void Clickselect(GameObject unitToAdd)
+    
+
+    
+
+    //public void shiftClickselect (GameObject unitToAdd)
+    
+
+   
+=======
+   // void Awake()
+    //{
+       // if (instance! - null && instance! - this)
+       // {
+           // Destroy(this.gameObject);
+        //}
+       // else
+        //{
+       //     instance = this;
+       // }
+   // }
+
+  //  public void Clickselect(GameObject unitToAdd)
+   // {
+
+   // }
+
+>>>>>>> Stashed changes
   //  public void shiftClickselect (GameObject unitToAdd)
   //  {
   
     //}
+<<<<<<< Updated upstream
+=======
+>>>>>>> 8e323b4824ffe4c23386b0352c4b83ab3f2fd66b
+>>>>>>> Stashed changes
 }
